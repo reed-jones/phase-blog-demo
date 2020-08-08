d6d41ea7ed3bf6c5b6ef4316c85c07ac112c13d8
Reed Jones <reedjones@reedjones.com>
2020-08-07 21:26:19 -0600

    version bump 0.5.1 (#7)
    * version bump 0.5.0
    
    * 0.5.1 phase
    
    * remove public folders
    
    * added vercel composer scripts
    
    * added stylesheet to head
    
    * filsystem
    
    * routing
    
    * added stylesheet to git
    
    * wip ci skip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip

---
+++ api/assets.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Built assets aren't currently routeable via vercel-php
+ */
+if ($_GET['type'] === 'css') {
+    header("Content-type: text/css; charset: UTF-8");
+    echo require __DIR__ . '/../public/css/' . basename($_GET['file']);
+} else if ($_GET['type'] === 'js') {
+    header('Content-Type: application/javascript; charset: UTF-8');
+    echo require __DIR__ . '/../public/js/' . basename($_GET['file']);
+}
--- resources/views/parts/head.blade.php
+++ resources/views/parts/head.blade.php
@@ -3,6 +3,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="Description" content="Phase Blog Demo">
 <title>{{ config('app.name') }}</title>
+<link rel="stylesheet" href="{{ mix('css/app.css') }}">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
 <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Inter:wght@200;400;700&display=swap" />
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@200;400;700&display=swap" media="print" onload="this.media='all'" />
