4a81e493fe4ae2b0dd0085a65c412c5f84e21f49
Reed Jones <reed.jones@pderas.com>
2020-08-07 21:04:23 -0600

    wip

--- api/assets.php
+++ api/assets.php
@@ -2,8 +2,8 @@
 
 if ($_GET['type'] === 'css') {
     header("Content-type: text/css; charset: UTF-8");
-    echo require __DIR__ . '/../public/css/' . $_GET['file'];
+    echo require __DIR__ . '/../public/css/' . basename($_GET['file']);
 } else if ($_GET['type'] === 'js') {
     header('Content-Type: application/javascript; charset: UTF-8');
-    echo require __DIR__ . '/../public/js/' . $_GET['file'];
+    echo require __DIR__ . '/../public/js/' . basename($_GET['file']);
 }
